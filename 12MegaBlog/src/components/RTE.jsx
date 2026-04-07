5d70ad9e696d131ed28950a56a1466b2273d17f5
unknown <thapa.ambica@gmail.com>
2026-04-07 17:29:57 +0530

    debug changes on postform

--- 12MegaBlog/src/components/RTE.jsx
+++ 12MegaBlog/src/components/RTE.jsx
@@ -12,6 +12,7 @@ export default function RTE(name, control, label, defaultValue="") {
         control={control}
         render={({field: {onChange}}) => (
             <Editor
+        value={value}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
--- 12MegaBlog/src/pages/Home.jsx
+++ 12MegaBlog/src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     useEffect(() => {
         appwriteService.getPosts().then((posts) => {
             if (posts) {
-                setPosts(posts.documents)
+                setPosts(posts?.documents || [])
             }
         })
     }, [])
@@ -20,7 +20,7 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login to read posts
+                                No posts available
                             </h1>
                         </div>
                     </div>
